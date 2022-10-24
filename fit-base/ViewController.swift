80a4fd8d54cb88ec4e8877f65ff3d622ae18eb0a
Shlyakhovenko Vladislav <homyak603@gmail.com>
2022-10-24 17:22:43 +0300

    build(app): initialize project

---
+++ fit-base/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fit-base
+//
+//  Created by Владислав Шляховенко on 10/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
